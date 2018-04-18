777c2f558c1145b25b59e37a1a513efe1d51315e
htao <htao@thoughtworks.com>
2018-04-18 15:27:21 +0800

    [taohui] display all employees in jsp page

--- src/main/java/com/example/employeeapi/controller/EmployeeController.java
+++ src/main/java/com/example/employeeapi/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.example.employeeapi.controller;
 
 import com.example.employeeapi.datasource.EmployeeRepository;
+import com.example.employeeapi.datasource.TestEmployeeRepository;
+import com.example.employeeapi.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
@@ -9,20 +11,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @EnableAutoConfiguration
 public class EmployeeController {
     @Autowired
-    private EmployeeRepository employeeRepository;
+    private EmployeeRepository employeeRepository = new TestEmployeeRepository();
 
     @RequestMapping("/employees")
     String getEmployees(Model model) {
-
-        model.addAttribute("message", "hello world");
-
+        List<Employee> employees = employeeRepository.getAllEmployees();
+        model.addAttribute("employees", employees);
         return "employees";
     }
-
 
     @GetMapping("/employees/{id}")
     ResponseEntity getEmployee(@PathVariable("id") int id) {
--- src/main/webapp/WEB-INF/jsp/employees.jsp
+++ src/main/webapp/WEB-INF/jsp/employees.jsp
@@ -1,15 +1,36 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
          pageEncoding="UTF-8" %>
+<%@page import="com.example.employeeapi.model.Employee"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>员工列表</title>
+    <style type="text/css">
+        .header{
+            background-color: black;
+            color: white;
+        }
+    </style>
 </head>
 <body>
-
-<div>${message}</div>
-
+<table>
+    <tr class="header">
+        <td>编号</td>
+        <td>姓名</td>
+        <td>年龄</td>
+        <td>性别</td>
+    </tr>
+    <c:forEach items="${employees}" var="employee">
+        <tr>
+            <td>${employee.id}</td>
+            <td>${employee.name}</td>
+            <td>${employee.age}</td>
+            <td>${employee.gender}</td>
+        </tr>
+    </c:forEach>
+</table>
 </body>
 </html>
